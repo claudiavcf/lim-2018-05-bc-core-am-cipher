5eaf52f3c873f2a98c44c893c3de1e8ae0fba46a
claudiavcf <ccf1693@gmail.com>
2018-05-23 10:33:07 -0500

    actualizando cambios

--- src/cipher.js
+++ src/cipher.js
@@ -9,4 +9,26 @@ window.cipher = {
   }
 };
 
+document.getElementById('Encriptado').onclick=Encriptado
+document.getElementById('Desencriptado').onclick=Desencriptado
+document.getElementById('Enviar').onclick=Enviar
+
+function Encriptado(){
+  
+  //var palabra= document.getElementById("txtarea");
+  //alert(palabra[0].value);
+
+  var palabra= document.getElementById("txtarea").value;
+  alert(palabra)
+}
+
+function Desencriptado(){
+  alert("Se realizó el desencriptado")
+}
+
+function Enviar(){
+  alert("Se envió el mensaje")
+}
+
+
 //funciones
