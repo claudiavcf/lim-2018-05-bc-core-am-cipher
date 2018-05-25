e45268e2bf9078df45171355894d8dfd19eca10d
claudiavcf <ccf1693@gmail.com>
2018-05-25 10:14:50 -0500

    Actualizando

--- src/cipher.js
+++ src/cipher.js
@@ -1,73 +1,56 @@
-alert('aqui esta mi funcion de decode y encode');
 
 window.cipher = {
-  //
-  // ... window es un objeto que toda variable o funcion se vuelve global
- // const y let nunca se declaran de manera global, por tanto no se encuentra dentro del objeto window, en caso de var, si 
-
-  //const cipher
-  encode : (offset, string) => {
-  string.toupperCase();
-  }
+ 
 };
 
-document.getElementById('Encriptado').addEventListener('click',Encriptado)
-document.getElementById('Desencriptado').onclick=Desencriptado
-document.getElementById('Digito').value=0
-document.getElementById('Enviar').addEventListener('click',Enviar)
-
-function Encriptado(){
+const Encriptado=()=>{
   
-  //var palabra= document.getElementById("txtarea");
-  //alert(palabra[0].value);
 
-  //let desencriptado= document.getElementById("txtarea").value.toupperCase();
-  let desencriptado= document.getElementById("txtarea");
-  //let encriptado= document.getElementById("Encriptado");
-  let dig=document.getElementById("Digito");
-  let cipherArray= new Array();
-  [...desencriptado].forEach(char => {
-    if (char.charCodeAt() == 32){
-      cipherArray.push(32);
-    } else {
-      charNumber = char.charCodeAt() - 65;
-      cipherChar= (charNumber + parseInt(dig.value)) % 26;
-      cipherArray.push(cipherChar +  65);
-    }
+    let textoDesencriptado = document.querySelector('#txtarea').value.toUpperCase();
+    let digito = document.querySelector('#Digito');
+    let cipherArray = new Array();
+    [...textoDesencriptado].forEach(char => {
+        if (char.charCodeAt() === 32) {
+            cipherArray.push(32);
+        } else {
+            charNumber = char.charCodeAt() - 65;
+            cipherChar = (charNumber + parseInt(digito.value)) % 26;
+            cipherArray.push(cipherChar + 65);
+        }
+    })
+    document.getElementById("resultado").innerHTML = String.fromCharCode(...cipherArray);
+
+    
+
+}
+
+const Desencriptado=()=>{
+  
+  let textoEncriptado= document.querySelector('#txtarea').value.toUpperCase();
+  let digito = document.querySelector('#Digito');
+  let cipherArray = new Array();
+  [...textoEncriptado].forEach(char => {
+      if (char.charCodeAt() === 32) {
+          cipherArray.push(32);
+      } else {
+          charNumber = char.charCodeAt() - 65;
+          cipherChar = (charNumber - parseInt(digito.value));
+          if (cipherChar > 25 || cipherChar < 0) cipherChar = cipherChar + 26
+          cipherChar = cipherChar % 26
+          cipherArray.push(cipherChar + 65);
+      }
   })
-
   document.getElementById("resultado").innerHTML = String.fromCharCode(...cipherArray);
 
 }
 
-/*function Desencriptado(){
-  
-  let desencriptado= document.getElementById("txtarea");
-  //let encriptado= document.getElementById("Encriptado").value.toupperCase();
- // let encriptado= document.getElementById("Encriptado");
-  let dig=document.getElementById("Digito");
-  let cipherArray= new Array();
-  [...desencriptado].forEach(char => {
-    if (char.charCodeAt() == 32){
-      cipherArray.push(32);
-    } else {
-      charNumber = char.charCodeAt() - 65;
-      cipherChar= (charNumber - parseInt(dig.value));
-      
-      if (cipherChar > 25||cipherChar < 0) cipherChar= cipherChar + 26
-      cipherChar= cipherChar % 26
-      cipherArray.push(cipherChar +  65);
-    }
-  })
+const Limpiar=()=>{
+    txtarea.value = "";
+   resultado.value = "";
+    Digito.value = 1;
+}
 
-  document.getElementById("resultado").innerHTML = String.fromCharCode(...cipherArray);
-
-
-}*/
-
-function Enviar(){
+const Enviar=()=>{
   alert("Se envió el mensaje")
 }
 
-
-//funciones
--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
-alert('aqui voy a manipular el dom')
 
-//document getElementById
+
+document.getElementById('Encriptado').addEventListener('click',Encriptado)
+document.getElementById('Desencriptado').addEventListener('click',Desencriptado)
+document.getElementById('Digito').value=1
+document.getElementById('Limpiar').addEventListener('click',Limpiar)
+document.getElementById('Enviar').addEventListener('click',Enviar)
