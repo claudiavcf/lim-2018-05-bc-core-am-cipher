332ba7704b78b52f5c408ed48838884a3816e28a
claudiavcf <ccf1693@gmail.com>
2018-05-31 13:03:17 -0500

    Actualizando....

--- src/cipher.js
+++ src/cipher.js
@@ -1,56 +1,43 @@
-
 window.cipher = {
- 
-};
+	encode: (offset, txtarea) => {
+		//let es una variable que va cambiando, a inicio " " luego se agrega texto
+		//const es una variable ,cuando guardas una formula en esa variable
+		let resultadoCifrado = '';
+		let codigoCaracter;
+		let nuevoCodigo;
+		let caracterCifrado;
 
-const Encriptado=()=>{
-  
+		for (let indice = 0; indice < txtarea.length; indice++) {
+			codigoCaracter = txtarea.toUpperCase().charCodeAt(indice); //obtiene letra  y lo convierte a numero
+			if (codigoCaracter == 32) {
+				caracterCifrado = " ";
+				resultadoCifrado = resultadoCifrado + caracterCifrado;
+			} else {
+				nuevoCodigo = (codigoCaracter - 65 + offset) % 26 + 65; //suma el numero convertido con el offset
+				caracterCifrado = String.fromCharCode(nuevoCodigo); //convierte el nuevo numero a letra
+				resultadoCifrado = resultadoCifrado + caracterCifrado;
+			}
+		}
+		return resultadoCifrado;
+	},
 
-    let textoDesencriptado = document.querySelector('#txtarea').value.toUpperCase();
-    let digito = document.querySelector('#Digito');
-    let cipherArray = new Array();
-    [...textoDesencriptado].forEach(char => {
-        if (char.charCodeAt() === 32) {
-            cipherArray.push(32);
-        } else {
-            charNumber = char.charCodeAt() - 65;
-            cipherChar = (charNumber + parseInt(digito.value)) % 26;
-            cipherArray.push(cipherChar + 65);
-        }
-    })
-    document.getElementById("resultado").innerHTML = String.fromCharCode(...cipherArray);
+	decode: (offset, txtarea) => {
+		let resultadoDescifrado = '';
+		let codigoLetraActual;
+		let codigoLetraAnterior;
+		let letraAnterior;
 
-    
-
+		for (let indice = 0; indice < txtarea.length; indice++) {
+			codigoLetraActual = txtarea.toUpperCase().charCodeAt(indice); //obtiene letra y convierte a numero
+			if (codigoLetraActual == 32) {
+				letraAnterior = " ";
+				resultadoDescifrado = resultadoDescifrado + letraAnterior;
+			} else {
+				codigoLetraAnterior = (codigoLetraActual + 65 - offset) % 26 + 65; //resta el numero convertido con el offset
+				letraAnterior = String.fromCharCode(codigoLetraAnterior); //convierte el numero a letra
+				resultadoDescifrado = resultadoDescifrado + letraAnterior;
+			}
+		}
+		return resultadoDescifrado;
+	},
 }
-
-const Desencriptado=()=>{
-  
-  let textoEncriptado= document.querySelector('#txtarea').value.toUpperCase();
-  let digito = document.querySelector('#Digito');
-  let cipherArray = new Array();
-  [...textoEncriptado].forEach(char => {
-      if (char.charCodeAt() === 32) {
-          cipherArray.push(32);
-      } else {
-          charNumber = char.charCodeAt() - 65;
-          cipherChar = (charNumber - parseInt(digito.value));
-          if (cipherChar > 25 || cipherChar < 0) cipherChar = cipherChar + 26
-          cipherChar = cipherChar % 26
-          cipherArray.push(cipherChar + 65);
-      }
-  })
-  document.getElementById("resultado").innerHTML = String.fromCharCode(...cipherArray);
-
-}
-
-const Limpiar=()=>{
-    txtarea.value = "";
-   resultado.value = "";
-    Digito.value = 1;
-}
-
-const Enviar=()=>{
-  alert("Se envió el mensaje")
-}
-
--- src/index.js
+++ src/index.js
@@ -1,7 +1,28 @@
+let btnCifrar =  document.getElementById('Cifrado');
+btnCifrar.addEventListener("click",() => {
+    let offset = parseInt(document.getElementById("txtOffset").value);
+    let txtarea = document.getElementById("txtarea").value;
+    let encode = cipher.encode(offset, txtarea);
+    document.getElementById("resultado").innerHTML = encode;
+} );
+
+let btnDescifrar = document.getElementById('Descifrado');
+btnDescifrar.addEventListener("click",() => {
+    let offset = parseInt(document.getElementById("txtOffset").value);
+    let txtarea = document.getElementById("txtarea").value;
+    let decode = cipher.decode(offset, txtarea);
+    document.getElementById("resultado").innerHTML = decode;
+});
 
 
-document.getElementById('Encriptado').addEventListener('click',Encriptado)
-document.getElementById('Desencriptado').addEventListener('click',Desencriptado)
-document.getElementById('Digito').value=1
-document.getElementById('Limpiar').addEventListener('click',Limpiar)
-document.getElementById('Enviar').addEventListener('click',Enviar)
+let btnLimpiar = document.getElementById("Limpiar");
+btnLimpiar.addEventListener("click",() => {
+    document.getElementById("txtarea").value = "";
+    
+
+}); 
+
+let btnEnviar = document.getElementById("Enviar");
+btnEnviar.addEventListener("click",()=> {
+    alert("Mensaje enviado")
+});
